4b82aa0b20fc1e6e433de74a63ef6fabfb1bc14e
Aryan Kumar <iamaryan.hzb@gmail.com>
2026-09-22 21:08:54 +0530

    feat: add blank lines for improved readability in TicketServiceApplication

--- services/ticket-service/src/main/java/com/ticketflow/ticket_service/TicketServiceApplication.java
+++ services/ticket-service/src/main/java/com/ticketflow/ticket_service/TicketServiceApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TicketServiceApplication {
 
+
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TicketServiceApplication.class, args);
 	}
